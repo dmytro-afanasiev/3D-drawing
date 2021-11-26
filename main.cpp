9b560919dc4eb724730729f1c5d0eb711de888e0
Dmytro Afanasiev <duma12let@gmail.com>
2021-11-26 21:11:09 +0200

    Add some gradient

--- main.cpp
+++ main.cpp
@@ -17,6 +17,18 @@ void print_to_coordinates(int x, int y, char c)
     printf("\033[%d;%dH%c", x, y, c);
 }
 
+float cut(float value, float min, float max){
+    if (value < min)
+        return min;
+    else if (value > max)
+        return max;
+    else return value;
+}
+
+float distance_from_centre(float x, float y){
+    return sqrt(x*x + y*y);
+}
+
 int main()
 {
     winsize w = get_resolution();
@@ -24,23 +36,29 @@ int main()
     int width = w.ws_col;
     float relation = (float)width / height;
     float pixel_relation = 11.0f / 23.0f;
+    char gradient[] = " .:!/r(l1Z4H9W8$@";
+    int gradient_size = std::size(gradient) - 2;
 
     float t = 0;
+    //while (true){
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             float y = (float)i / height * 2.0f - 1.0f;
             float x = (float)j / width * 2.0f - 1.0f;
-
             x *= relation * pixel_relation;
+            //x += sin(t*0.005);
             char pixel = ' ';
-            if (x*x + y*y < 0.5)
-            {
-                pixel = '@';
-            }
+            float distance_to_point = distance_from_centre(x, y);
+        
+            int color = (int)(0.5f/ distance_to_point);
+            color = cut(color, 0, gradient_size);
+            pixel = gradient[color];
             print_to_coordinates(i, j, pixel);
         }
+    //}
+    t++;
     }
 
     return 0;
